e769465d915f70c70d72da4a426f5762c7cd1816
Marcus Ramos <marcus.vinicius@rubcube.com>
2024-10-15 11:22:22 -0300

    chore: improving template method

--- src/account/controller/account-controller.ts
+++ src/account/controller/account-controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
+import { Account } from "../entities/account";
+import { AccountReceivableController } from "./account-receivable-controller";
+import { AccountToPayController } from "./account-to-pay-controller";
 
 export abstract class AccountController { 
+  protected bankAccount: Account | undefined;
+  protected enterpriseAccount: Account | undefined;
+
   public async settleAccount(req: Request, res: Response): Promise<Response> {
     try {
-      this.validateAccount(req);    
+      const isValid = this.validateAccount(req); 
+      if(!isValid){
+        return res.status(400).send("Insuficient balance")
+      }   
       await this.processPayment(req);
       return this.sendConfirmation(res);
     }
@@ -12,7 +21,32 @@ export abstract class AccountController {
     }
   }
 
-  abstract validateAccount(req: Request): void;
+  protected async initializeAccounts(): Promise<void> {
+    const [enterpriseAccount, bankAccount] = await Promise.all([
+      Account.getAccount("empresa"),
+      Account.getAccount("banco")
+    ]);
+  
+    if (!enterpriseAccount || !bankAccount) {
+      throw new Error("Banco de dados incompleto!");
+    }
+
+    this.enterpriseAccount = enterpriseAccount;
+    this.bankAccount = bankAccount;
+  }
+
+  protected validateAccount(req: Request): boolean{
+    const amount = req.body.amount;
+    if(this instanceof AccountReceivableController){
+      return !!this.bankAccount?.validateBalance(amount);
+    }
+
+    if(this instanceof AccountToPayController){
+      return !!this.enterpriseAccount?.validateBalance(amount);
+    }
+
+    return false;
+  };
 
   abstract processPayment(req: Request): Promise<void>;
 
--- src/account/controller/account-receivable-controller.ts
+++ src/account/controller/account-receivable-controller.ts
@@ -3,45 +3,17 @@ import { AccountController } from "./account-controller";
 import { Request, Response } from "express";
 
 export class AccountReceivableController extends AccountController {
-  private destinationAccount: Account | undefined;
-  private originAccount: Account | undefined;
-
   public constructor() {
     super();
     this.initializeAccounts()
-      .catch(error => {
-        throw new Error("Failed to initialize accounts.");
-      });
   }
 
-  private async initializeAccounts(): Promise<void> {
-    const [enterprise, bank] = await Promise.all([
-      Account.getAccount("empresa"),
-      Account.getAccount("banco")
-    ]);
 
-    if (!enterprise || !bank) {
-      throw new Error("Banco de dados incompleto!");
-    }
-
-    this.destinationAccount = enterprise;
-    this.originAccount = bank;
-  }
 
   public async processPayment(req: Request): Promise<void> {
     const amount = req.body.amount;
-    await this.originAccount?.decreaseBalance(amount);
-    await this.destinationAccount?.increaseBalance(amount);
-  }
-
-  public validateAccount(req: Request): void {
-    const amount = req.body.amount;
-    if(!amount){
-      throw new Error("Amount não informado");
-    }
-    if(!this.originAccount?.validateBalance(amount)){
-      throw new Error("Saldo insuficiente!")
-    };
+    await this.bankAccount?.decreaseBalance(amount);
+    await this.enterpriseAccount?.increaseBalance(amount);
   }
 
   public sendConfirmation(res: Response): Response {
